17f43dcc2a7d75aad6ae9abe637d5862b79e19fd
Jaswanth Remiel AG <66111735+JaswanthRemiel@users.noreply.github.com>
2022-04-16 10:58:40 +0530

    Create RPC Discord YT#1.py

---
+++ RPC Discord YT#1.py
@@ -0,0 +1,16 @@
+from pypresence import Presence
+import time
+start = int(time.time())
+client_id = "964552321113669722"  
+RPC = Presence(client_id)
+RPC.connect()
+while True: #infinite loop
+    RPC.update(
+        large_image = "rebrand-jr", #name of your image in Developer Portal
+        large_text = "RV Logo", #Alt Text For Your Logo 
+        details = "Chillin' With Code", #Set Details
+        state = "Visual Studio Code", #Set State
+        start = start,
+        buttons = [{"label": "Remiel's Vlog", "url": "https://www.youtube.com/remielvlogsyt"}, {"label": "Discord Server", "url": "https://discord.gg/rgAZT2aCCH"}] #Add Buttons Here
+    )
+    time.sleep(60)
